787dd6eac3f2385de1d974d35e6c1ac4986a994b
kenzodiac <kennethfujimura@gmail.com>
2023-03-17 04:31:37 -0700

    readme edit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@
 // "Social Media Dashboard in React"
 // This is an assignment where we built out the front end of a social media dashboard website in React. We used components to reduce the amount of code significantly, and passed in variables through props. 
 // Peer Reviewed By: _______ -- "_______"
-//
+// Github Repo: https://github.com/kenzodiac/socialmediadashboard
+// Azure Deployment: https://nothingofvaluedashboard.azurewebsites.net/
 
 import './App.css';
 import MainStageComponent from './Components/MainStageComponent';
